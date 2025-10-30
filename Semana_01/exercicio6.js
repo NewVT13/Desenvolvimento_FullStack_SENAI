bb7294207373714c706f0c3ca10edd97166d2478
NewVT13 <105737109+NewVT13@users.noreply.github.com>
2025-10-30 12:54:42 -0300

    Refactor calculator logic for better readability

--- Semana_01/exercicio6.js
+++ Semana_01/exercicio6.js
@@ -3,23 +3,30 @@ const readlineSync = require('readline-sync');
 console.log(">>>>>>>>>>Calculadora<<<<<<<<<<")
 
 var numeroUm = readlineSync.questionFloat("DIGITE UM NUMERO: ")
-var numeroDois= readlineSync.questionFloat("DIGITE OUTRO NUMERO: ")
+var numeroDois = readlineSync.questionFloat("DIGITE OUTRO NUMERO: ")
 var operador = readlineSync.question("DIGITE UM OPERADOR SOMA, SUB, MULT, OU DIVI: ")
 
-if(operador.toUpperCase() == "SOMA"){
-    console.log("O RESULTADO É: " + numeroUm + numeroDois);
+if (operador.toUpperCase() == "SOMA") {
+    var resultado = numeroUm + numeroDois
+    console.log("O RESULTADO É: " + resultado);
 }
-else if(operador.toUpperCase() == "SUB"){
-    console.log("O RESULTADO É: " + numeroUm - numeroDois);
+else if (operador.toUpperCase() == "SUB") {
+    var resultado = numeroUm - numeroDois
+    console.log("O RESULTADO É: " + resultado);
 }
-else if(operador.toUpperCase() == "MULT"){
-    console.log("O RESULTADO É: " + numeroUm * numeroDois);
+else if (operador.toUpperCase() == "MULT") {
+    var resultado = numeroUm * numeroDois
+    console.log("O RESULTADO É: " + resultado);
 }
-else if(operador.toUpperCase() == "DIVI"){
-    console.log("O RESULTADO É: " + numeroUm / numeroDois);
+else if (operador.toUpperCase() == "DIVI") {
+    if (numeroDois == 0) {
+        console.log("Não é possível dividir por 0")
+    } else {
+        var resultado = numeroUm / numeroDois
+        console.log("O RESULTADO É: " + resultado.toFixed(2));
+    }
 }
-else{
+else {
     console.log("VOCÊ DIGITOU ALGO ERRADO, DIGITE NOVAMENTE O OPERADOR CORRETO.")
     console.log(">>>>>>>>>>Fim do programa<<<<<<<<<<")
 }
-
