e99bef67f112761f9a3d585ca130324f4a3567fb
NewVT13 <viniciustmoura13@gmail.com>
2025-11-03 19:25:40 -0300

    corrgindo o exercicio

--- Semana_01/exercicio7.js
+++ Semana_01/exercicio7.js
@@ -1,8 +1,11 @@
 const readlineSync = require('readline-sync');
 
+var altura = readlineSync.questionFloat("Qual é a sua altura? ")
+var peso = readlineSync.questionFloat("Qual é seu peso? ")
+
 function calcularImc (altura, peso){
     return peso / (altura * altura)
 }
 
-console.log(calcularImc(1.70, 75))
+console.log(calcularImc(altura, peso))
 
