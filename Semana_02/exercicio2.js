1e66208a6338d8fb56543c12d19f8dfffa583402
NewVT13 <viniciustmoura13@gmail.com>
2025-11-04 11:48:16 -0300

    Projeto alterado

--- Semana_02/exercicio2.js
+++ Semana_02/exercicio2.js
@@ -1,2 +1,17 @@
 const readlineSync = require('readline-sync');
 
+function alterarNotas(inserirNota) {
+    if (inserirNota >= 9) {
+        return "A - Aprovado"
+    } else if (inserirNota >= 7 && inserirNota <= 8.9) {
+        return "B - Bom"
+    } else if (inserirNota >= 5 && inserirNota <= 6.9) {
+        return "C - Suficiente"
+    } else {
+        return "D - Reprovado"
+    }
+}
+
+var inserirNota = readlineSync.questionFloat("Qual e a nota do aluno? ")
+
+console.log("Sua nota foi " + alterarNotas(inserirNota))
