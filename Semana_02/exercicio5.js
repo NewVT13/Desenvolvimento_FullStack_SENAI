b9a38136801c9f1033eba4eecd9b0e44daa9e12b
NewVT13 <viniciustmoura13@gmail.com>
2025-11-07 11:08:25 -0300

    completada a resolução

--- Semana_02/exercicio5.js
+++ Semana_02/exercicio5.js
@@ -1,10 +1,12 @@
 const readlineSync = require('readline-sync');
 
-var ladoA = readlineSync.questionFloat("Digite o Lado A")
-var ladoB = readlineSync.questionFloat("Digite o Lado B")
-var ladoC = readlineSync.questionFloat("Digite o Lado C")
+var ladoA = readlineSync.questionFloat("Digite o Lado A ")
+var ladoB = readlineSync.questionFloat("Digite o Lado B ")
+var ladoC = readlineSync.questionFloat("Digite o Lado C ")
 
-if (ladoA == ladoB && ladoB == ladoC) {
+if(ladoA + ladoB < ladoC || ladoA + ladoC < ladoB || ladoB + ladoC < ladoA){
+    console.log("Não é um triangulo valido")
+}else if (ladoA == ladoB && ladoB == ladoC) {
     console.log("Equilatero")
 } else if (ladoA != ladoB && ladoB != ladoC){
     console.log("Escaleno")
