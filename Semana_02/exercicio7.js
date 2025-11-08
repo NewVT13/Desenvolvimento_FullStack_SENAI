8784516e174138cab3d72e9f12fed6615531516a
NewVT13 <viniciustmoura13@gmail.com>
2025-11-08 12:29:51 -0300

    corrigido e melhorado o código

--- Semana_02/exercicio7.js
+++ Semana_02/exercicio7.js
@@ -1,5 +1,6 @@
-var texto = "A empresax, voces tem um funcionario ladrão que tentou burlar o sistema de segurança para que a empresax fosse difamada."
-
+var texto = "Contato Joana, CPF:506.967.532-23: A empresax, voces tem um funcionario ladrão que tentou burlar o sistema de segurança para que a empresax fosse difamada."
+var cpfRegex = /\b\d{3}\.?\d{3}\.?\d{3}-?\d{2}\b/g;
+/*
 if (texto.includes("empresax") && texto.includes("ladrão") &&
     texto.includes("burlar")) {
     textoCorrigido = texto.replaceAll("empresax", "[Razão Social]").replaceAll("ladrão", "[Mau-caráter]").replaceAll("burlar", "[Adulterar]")
@@ -7,5 +8,20 @@ if (texto.includes("empresax") && texto.includes("ladrão") &&
 } else {
     console.log(texto)
 }
+*/
 
+function corrigirTexto (texto) {
+    if (texto.includes("empresax") && 
+    texto.includes("ladrão") &&
+    texto.includes("burlar")) {
+    textoCorrigido = texto.replaceAll("empresax", "[Razão Social]")
+    .replaceAll("ladrão", "[Mau-caráter]")
+    .replaceAll("burlar", "[Adulterar]")
 
+    return textoCorrigido
+} else {
+    return texto
+}
+}
+
+console.log(corrigirTexto(texto.replaceAll(cpfRegex, "[CPF]")))
