7478d81ae05862001b1d72a307fa29ed05937936
NewVT13 <viniciustmoura13@gmail.com>
2026-01-26 11:07:04 -0300

    Adicionado pastas de front-end e movida o restante do proeto para a pasta introdução a programação

---
+++ Introdução a Programação/Semana_05/exercicio1.js
@@ -0,0 +1,6 @@
+const readlineSync = require('readline-sync');
+
+var quantidade_pao = readlineSync.questionInt("Digite a quantidade de pao")
+var quantidade_carne = readlineSync.questionInt("Digite a quantidade de carne")
+var quantidade_queijo_chedar = readlineSync.questionInt("Digite a quantidade de queijo chedar")
+
