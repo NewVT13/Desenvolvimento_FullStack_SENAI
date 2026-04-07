e2a38c83a860f25d11062513512729f7bc353d82
Vinicius Tarouquela <viniciustmoura13@gmail.com>
2026-04-06 22:04:08 -0300

    Alterada a pasta para semana 09 e inicializado a loja com react pelo vite

---
+++ Front-end/Semana_10/loja_react/loja/src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { FaRegUser } from "react-icons/fa";
+import { RiLockPasswordLine } from "react-icons/ri";
+
+function App() {
+	return (
+		<div className="container_home">
+			<div className="container_esquerda">
+				<h2>Welcome back!</h2>
+				<p>you can sign in to acess with yout existing account.</p>
+			</div>
+			<div className="container_direita">
+				<h1>Sing in</h1>
+				<div className="container_input">
+					<FaRegUser size={20} color="#ccc" />
+					<input placeholder="User email" />
+				</div>
+				<div className="container_input">
+					<RiLockPasswordLine size={20} color="#ccc" />
+					<input placeholder="Password" />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
