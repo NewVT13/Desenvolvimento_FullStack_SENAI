2a122fe6baed581e1a3b1f9e59c42f2bf9a5e103
Vinicius Tarouquela <viniciustmoura13@gmail.com>
2026-05-27 08:44:27 -0300

    adicionado código de db com arquivos do db

--- back-end/Semana_01/meu_primeiro_servidor/src/main.js
+++ back-end/Semana_01/meu_primeiro_servidor/src/main.js
@@ -45,6 +45,85 @@ app.post("/clientes", (request, response) => {
 	}
 });
 
+app.delete("/clientes/:id", async (request, response) => {
+	const id = Number(request.params.id);
+	const clienteExiste = db.data.clientes.some(cliente => cliente.id === id);
+
+	if (!clienteExiste) {
+		return response.status(404).send({ error: "Cliente não encontrado" });
+	}
+
+	const clientesFiltrados = db.data.clientes.filter(
+		cliente => cliente.id !== id
+	);
+	db.data.clientes = clientesFiltrados;
+	await db.write();
+
+	response.send({ mensagem: "Cliente deletado com sucesso" });
+});
+
+app.put("/clientes/:id", async (request, response) => {
+	const idCliente = Number(request.params.id); // 5
+	const dadosNoBody = request.body; // {nome: "joaquim"}
+
+	/* ETAPA  1 - VALIDICACAO */
+	if (
+		dadosNoBody.nome !== undefined &&
+		(typeof dadosNoBody.nome !== "string" || dadosNoBody.nome.trim() === "")
+	) {
+		response.status(400).send({
+			error: "O nome não pode ser vazio",
+		});
+		return;
+	}
+	if (
+		dadosNoBody.salario !== undefined &&
+		(typeof dadosNoBody.salario !== "Number" || dadosNoBody.salario < 0)
+	) {
+		response.status(400).send({ error: "O salario precisa ser maior do que 0" });
+		return;
+	}
+	if (
+		dadosNoBody.habilitacao !== undefined &&
+		typeof dadosNoBody.habilitacao !== "boolean"
+	) {
+		response.status(400).send({ error: "A habilitação deve ser um booleano" });
+		return;
+	}
+
+	const clienteEncontrado = db.data.clientes.some(
+		cliente => cliente.id === idCliente
+	);
+
+	if (!clienteEncontrado) {
+		response.status(404).send({ error: "Cliente nao encontrado" });
+	} else {
+		// ETAPA 2 - MAPEAMENTO
+		const clientesAlterados = db.data.clientes.map(cliente => {
+			if (cliente.id === idCliente) {
+				if (dadosNoBody.nome !== undefined) {
+					cliente.nome = dadosNoBody.nome;
+				}
+
+				if (dadosNoBody.salario !== undefined) {
+					cliente.salario = dadosNoBody.salario;
+				}
+
+				if (dadosNoBody.habilitacao !== undefined) {
+					cliente.habilitacao = dadosNoBody.habilitacao;
+				}
+			}
+			return cliente;
+		});
+
+		// ETAPA 3 - PERSISTENCIA
+		db.data.clientes = clientesAlterados;
+		await db.write();
+
+		response.send({ messagem: "Dados atualizados com sucesso" });
+	}
+	/*FIM DA VALIDACAO */
+});
 app.listen(PORTA_APP, () => {
 	console.log("Rodando o server");
 });
